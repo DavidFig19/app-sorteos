a5eb4c50fa33a3dcb9886febe3898acc8273936b
josue1919 <52299245+josue1919@users.noreply.github.com>
2020-12-22 11:21:24 -0600

    rueda
    rueda

--- js/logic.js
+++ js/logic.js
@@ -1,37 +1,48 @@
-var participantes=[];
-var sonido=new Audio();
-sonido.src="../assets/sonido.mp3";
-var item;
-function Addplayer(){
-
-    //guardo en una variable el valor de el input
-    var persona=document.getElementById("texbox").value;
 
 
-    //agrego el valor del input a un
-    // persona = persona.replace(/(\r\n\t|\n|\r\t)/gm,"");
 
-    var persona=persona.split("\n");
-    participantes.push(persona);
-    console.log(participantes);
+
+
+
+
+
+
+
+
+// var participantes=[];
+// var sonido=new Audio();
+// sonido.src="../assets/sonido.mp3";
+// var item;
+// function Addplayer(){
+
+//     //guardo en una variable el valor de el input
+//     var persona=document.getElementById("texbox").value;
+
+
+//     //agrego el valor del input a un
+//     // persona = persona.replace(/(\r\n\t|\n|\r\t)/gm,"");
+
+//     var persona=persona.split("\n");
+//     participantes.push(persona);
+//     console.log(participantes);
 
     
 
       
     
-        // var ul = document.getElementById("list");
-        // var li = document.createElement("li");
+//         // var ul = document.getElementById("list");
+//         // var li = document.createElement("li");
       
-        //     li.appendChild(document.createTextNode(persona));
-        //     li.setAttribute("id", "list"); // added line
-        //     ul.appendChild(li);
+//         //     li.appendChild(document.createTextNode(persona));
+//         //     li.setAttribute("id", "list"); // added line
+//         //     ul.appendChild(li);
            
-        var ul = document.getElementById("list");
-        var li = document.createElement("li");
+//         var ul = document.getElementById("list");
+//         var li = document.createElement("li");
       
-            li.appendChild(document.createTextNode(persona));
-            li.setAttribute("id", "list"); // added line
-            ul.appendChild(li);
+//             li.appendChild(document.createTextNode(persona));
+//             li.setAttribute("id", "list"); // added line
+//             ul.appendChild(li);
            
             
       
@@ -40,57 +51,57 @@ function Addplayer(){
      
  
   
-    // alert(li.id);
-
-
-
-}
-
-
-
-// function Sortear(){
-//     var Indice=participantes.length();
-
-//     var Random=Math.floor(Math.random()*Indice)+1;
-//     console.log(Random);
-//     participantes.map((item)=>{
-
-//         item
-//     })
-
+//     // alert(li.id);
 
 
 
 // }
 
 
-// function Addplayer(){
 
-//     var persona=document.getElementById("texbox").value;
+// // function Sortear(){
+// //     var Indice=participantes.length();
 
+// //     var Random=Math.floor(Math.random()*Indice)+1;
+// //     console.log(Random);
+// //     participantes.map((item)=>{
 
-//     participantes.push(persona);
-//     console.log(participantes);
-
-//     participantes.forEach(participante => {
-//         // participante
-//         console.log(participante)
-//          // Inserta una fila en la tabla, en el índice 0
-//      // Obtiene una referencia a la tabla
-//      var tableRef = document.getElementById("tablaPersona");
-
-//     var newRow   = tableRef.insertRow(0);
-
-//     // Inserta una celda en la fila, en el índice 0
-//     var newCell  = newRow.insertCell(0);
-//     // Añade un nodo de texto a la celda
-//     var newText  = document.createTextNode(participante);
-//     newCell.appendChild(newText);
-//     console.log(newText)
-
-//     });
+// //         item
+// //     })
 
 
 
 
-// }
+// // }
+
+
+// // function Addplayer(){
+
+// //     var persona=document.getElementById("texbox").value;
+
+
+// //     participantes.push(persona);
+// //     console.log(participantes);
+
+// //     participantes.forEach(participante => {
+// //         // participante
+// //         console.log(participante)
+// //          // Inserta una fila en la tabla, en el índice 0
+// //      // Obtiene una referencia a la tabla
+// //      var tableRef = document.getElementById("tablaPersona");
+
+// //     var newRow   = tableRef.insertRow(0);
+
+// //     // Inserta una celda en la fila, en el índice 0
+// //     var newCell  = newRow.insertCell(0);
+// //     // Añade un nodo de texto a la celda
+// //     var newText  = document.createTextNode(participante);
+// //     newCell.appendChild(newText);
+// //     console.log(newText)
+
+// //     });
+
+
+
+
+// // }
