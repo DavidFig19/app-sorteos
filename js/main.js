206b5da667ad4d346ecfca2869af4166baea071a
josue1919 <52299245+josue1919@users.noreply.github.com>
2021-02-18 23:08:48 -0600

    sonido click

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-
 //declaramos un array para los participantes
 var participantes = [];
 
-
-
 var sonido = new Audio();
-sonido.src = "../assets/sonido.mp3"; //variable que hace el sonido del clik
+sonido.src = "../assets/mouse.mp3"; //variable que hace el sonido del clik
 //funcion para el sonido del clik
 function Click() {
   sonido.pause();
@@ -13,45 +10,7 @@ function Click() {
   sonido.play();
 }
 
-
-
-//vamos a validar cuando no hay nadie en la tabla
-function Validacion(){
- 
-
-  if(participantes.length===0){
-   
-
-    console.log("validado")
-
-    const CuerpoTabla = document.querySelector("#CuerpoTabla");
-    const aviso=document.createElement("h2");
-    aviso.textContent="Aun no hay participantes";
-    aviso.className="text-center"
-    aviso.setAttribute("id","aviso")
-    CuerpoTabla.appendChild(aviso)
-
-
-  }else{
-    if(participantes.length>1){
-      var borrar=document.querySelector("#CuerpoTabla").removeChild(aviso);
-      console.log(borrar)
-  
-    }
-  }
- 
-
-  
-
-  
-}
-
-Validacion();
-
-
-
 function Addplayer() {
-
   // guardo en una variable el valor de el input
   var persona = document.getElementById("persona").value;
   console.log(persona);
@@ -63,20 +22,28 @@ function Addplayer() {
   //para que se inserte individualmente los item seprarlos pro un foreach
 
   personas.forEach((item) => {
+    var d = new Date().getTime();
+    var uuid = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
 
-    const moonLanding = new Date();
-  const ID= moonLanding.getTime()+moonLanding.getMilliseconds()
-  console.log(ID)
+    //   const moonLanding = new Date();
+    // const ID= moonLanding.getTime()+moonLanding.getMilliseconds()
+    // console.log(ID)
     participantes.push({
-      
       nombre: item,
-      id: ID
+      id: uuid,
     });
-   
+
     console.log(participantes);
   });
-
-  CreateTable(participantes)
+  Click();
+  CreateTable(participantes);
 }
 
 function CreateTable(array) {
@@ -93,11 +60,11 @@ function CreateTable(array) {
     const tdNumero = document.createElement("td");
     //asignamos el valor de el array a la celda
     tdNombre.textContent = element.nombre;
-   
+
     //asignamos un valor con el id a la columna numero del participante
     tdNumero.textContent = element.id;
     //agregamosid
-    tr.setAttribute("id",element.id)
+    tr.setAttribute("id", element.id);
     //insertamos el nombre al tr
     tr.appendChild(tdNombre);
     //insertamos el numero al tr
@@ -107,20 +74,35 @@ function CreateTable(array) {
   });
 }
 
+function DeletePlayer() {
+  if (participantes.length == 0) {
+    alertify.error("Tabla vacia");
+  }
 
-
-function DeletePlayer(){
   //primer paso definimos el maximo del random
   //en base a la exension de el arreglo
-  var maximo=participantes.length;
-  console.log("maximo"+maximo)
+  var maximo = participantes.length;
+  console.log("maximo" + maximo);
   //creamos una constante random en base al maximo y minimo
-  const elejido= Math.floor(Math.random() * (maximo-0));
+  const elejido = Math.floor(Math.random() * (maximo - 0));
+
+  //mensaje del ganador
+  alertify
+    .alert()
+    .setting({
+      title: "Felicidades!!",
+      label: "entendido",
+      message: `Ganador: ${participantes[elejido].nombre}`,
+      // 'onok': function(){ alertify.success(`felicidades ${participantes[elejido].nombre}`);}
+    })
+    .show();
+
   //eliminamos de la tabla dependiendo de el id del participante elejido
   var CuerpoTabla = document.querySelector("#CuerpoTabla");
-  var eliminarhtml=document.getElementById(`${participantes[elejido].id}`);
-  var hijoeliminado=CuerpoTabla.removeChild(eliminarhtml);
+  var eliminarhtml = document.getElementById(`${participantes[elejido].id}`);
+  var hijoeliminado = CuerpoTabla.removeChild(eliminarhtml);
 
+  // alertify.success(`Ganador: ${participantes[elejido].nombre}`)
 
   // console.log("hijo eliminado: "+hijoeliminado);
   // console.log("el elejido es "+elejido);
@@ -128,10 +110,7 @@ function DeletePlayer(){
   // console.log(maximo);
 
   //eliminamos del arreglo el participante elejido
-  participantes.splice(elejido,1)
-
-  
- 
+  participantes.splice(elejido, 1);
 }
 const boton_agregar = document.getElementById("agregar");
 console.log(boton_agregar);
@@ -139,8 +118,7 @@ console.log(boton_agregar);
 boton_agregar.addEventListener("click", () => Addplayer(), true);
 
 //boton para sortear
-const boton_sortear=document.getElementById("sortear");
+const boton_sortear = document.getElementById("sortear");
 console.log(boton_sortear);
 
-
-boton_sortear.addEventListener('click',()=>DeletePlayer(),true);
+boton_sortear.addEventListener("click", () => DeletePlayer(), true);
